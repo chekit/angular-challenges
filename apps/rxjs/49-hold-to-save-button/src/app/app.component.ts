77250927d80aed552d552387b83b8f6df4cb3cfd
Anton Cherepov <chekitdnb@gmail.com>
2024-06-01 12:42:36 +0200

    feat(hold-to-save): add basic functionaity, wip

--- apps/rxjs/49-hold-to-save-button/src/app/app.component.ts
+++ apps/rxjs/49-hold-to-save-button/src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { SaveRequestComponent } from './components/save-request/save-request.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [SaveRequestComponent],
   selector: 'app-root',
-  template: `
-    <main class="flex h-screen items-center justify-center">
-      <div
-        class="flex w-full max-w-screen-sm flex-col items-center gap-y-8 p-4">
-        <button
-          class="rounded bg-indigo-600 px-4 py-2 font-bold text-white transition-colors ease-in-out hover:bg-indigo-700">
-          Hold me
-        </button>
-
-        <progress [value]="20" [max]="100"></progress>
-      </div>
-    </main>
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
-  onSend() {
-    console.log('Save it!');
-  }
-}
+export class AppComponent {}

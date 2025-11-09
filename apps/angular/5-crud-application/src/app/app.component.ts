b3f1c23749ec5f447437cc042ff1799c91f2c717
Anton Cherepov <chekitdnb@gmail.com>
2025-11-09 12:04:47 +0100

    fix: requests in service, immutable data, prettify rendering, data as signal

--- apps/angular/5-crud-application/src/app/app.component.ts
+++ apps/angular/5-crud-application/src/app/app.component.ts
@@ -1,50 +1,106 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
-import { randText } from '@ngneat/falso';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { finalize } from 'rxjs';
 import { ToDo } from './models/todo';
+import { TodoService } from './services/todo.service';
 
 @Component({
   imports: [],
   selector: 'app-root',
   template: `
-    @for (todo of todos; track todo.id) {
-      {{ todo.title }}
-      <button (click)="update(todo)">Update</button>
+    @for (todo of todos(); track todo.id) {
+      <div class="todo" [class.is-update]="todo.id === state().isUpdating">
+        <h2 class="todo__title">
+          {{ todo.title }}
+        </h2>
+        <button
+          class="todo__button"
+          (click)="update(todo)"
+          data-test="update-btn">
+          Update
+        </button>
+      </div>
     }
   `,
-  styles: [],
+  styles: [
+    `
+      :host {
+        display: block;
+        padding: 5px 15px;
+      }
+
+      .todo {
+        padding: 8px;
+        margin: 0 0 15px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+      }
+
+      .todo__title {
+        margin: 0 0 10px;
+        font-size: 18px;
+        font-weight: 800;
+      }
+
+      .todo__button {
+        padding: 5px 10px;
+        border: 1px soldi #aaa;
+        font-weight: 600;
+        cursor: pointer;
+      }
+
+      .is-update {
+        position: relative;
+        overflow: hidden;
+        pointer-events: none;
+      }
+
+      .is-update::after {
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.25);
+        text-align: center;
+        color: white;
+        content: 'Updating...';
+      }
+    `,
+  ],
 })
 export class AppComponent implements OnInit {
-  private http = inject(HttpClient);
+  private todoService = inject(TodoService);
 
-  todos!: ToDo[];
+  todos = signal<ToDo[]>([]);
+
+  protected state = signal({
+    isUpdating: -1,
+  });
 
   ngOnInit(): void {
-    this.http
-      .get<ToDo[]>('https://jsonplaceholder.typicode.com/todos')
-      .subscribe((todos) => {
-        this.todos = todos;
-      });
+    this.todoService.getTodoList().subscribe((todos) => {
+      this.todos.set(todos);
+    });
   }
 
   update(todo: ToDo) {
-    this.http
-      .put<ToDo>(
-        `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
-        JSON.stringify({
-          todo: todo.id,
-          title: randText(),
-          body: todo.body,
-          userId: todo.userId,
-        }),
-        {
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-        },
+    this.state.update((state) => ({ ...state, isUpdating: todo.id }));
+    this.todoService
+      .updateTodo(todo)
+      .pipe(
+        finalize(() =>
+          this.state.update((state) => ({ ...state, isUpdating: -1 })),
+        ),
       )
       .subscribe((todoUpdated: ToDo) => {
-        this.todos[todoUpdated.id - 1] = todoUpdated;
+        this.todos.update((todos) => [
+          ...todos.slice(0, todoUpdated.id - 1),
+          todoUpdated,
+          ...todos.slice(todoUpdated.id),
+        ]);
       });
   }
 }
